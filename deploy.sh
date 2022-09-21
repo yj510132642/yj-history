f3be48fae69e982a4c0fa23292e33a5302e56acc
yj510132642 <510132642@qq.com>
2022-09-21 14:09:56 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yj510132642/yj-history.git master:gh-pages
+git push -f git@github.com:yj510132642/yj-history.git main:jacky
 
 cd -
